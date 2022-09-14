1f4a265cbec447717086f2e818f275f63a218ee6
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-09-14 14:07:51 +0300

    Added db json file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect } from "react";
+import { hospitals } from "./db";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const showPosition = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position.coords.latitude);
+        console.log(position.coords.longitude);
+      });
+    }
+  };
+  useEffect(() => {
+    showPosition();
+  }, []);
+
+  const showAllHospitals = hospitals.map((hospital) => (
+    <div key={hospital.id}>
+      <h1>{hospital.name}</h1>
     </div>
-  );
+  ));
+
+  return <div className="App">{showAllHospitals}</div>;
 }
 
 export default App;
